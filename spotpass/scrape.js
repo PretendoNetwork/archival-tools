a4a5c12b20f35f5749ab4d0355e73f5a4b7f6e8e
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-01-27 11:11:20 -0500

    spotpass: add execution time log

--- spotpass/scrape.js
+++ spotpass/scrape.js
@@ -1,3 +1,4 @@
+const { performance } = require('node:perf_hooks');
 const fs = require('fs-extra');
 const scrapeWiiU = require('./scrape-wiiu');
 const scrape3DS = require('./scrape-3ds');
@@ -14,10 +15,29 @@ async function scrape() {
 	}
 
 	// * Run both at the same time
+	const startTime = performance.now();
+
 	await Promise.all([
 		scrapeWiiU(downloadBase),
 		scrape3DS(downloadBase)
 	]);
+
+	const endTime = performance.now();
+	const executionTime = millisecondsToString(endTime - startTime);
+
+	console.log(`Archived in ${executionTime}`);
+}
+
+function millisecondsToString(ms) {
+	const seconds = Math.floor((ms / 1000) % 60);
+	const minutes = Math.floor((ms / 1000 / 60) % 60);
+	const hours = Math.floor((ms  / 1000 / 3600 ) % 24)
+
+	return [
+		`${hours.toString().padStart(2, '0')}h`,
+		`${minutes.toString().padStart(2, '0')}m`,
+		`${seconds.toString().padStart(2, '0')}s`
+	].join(':');
 }
 
 scrape();
