7613b53e077552bb197a800734c0245a9a95d0dd
Jonathan Barrow <jonbarrow1998@gmail.com>
2024-02-03 15:06:46 -0500

    spotpass: removed duplicate countries

--- spotpass/constants.js
+++ spotpass/constants.js
@@ -1,20 +1,19 @@
 // * All country codes, courtesy of https://www.3dbrew.org/wiki/Country_Code_List
 const COUNTRIES = [
-	'GB', 'US', 'IT', 'NL', 'DE', 'CA', 'FR', 'HU', 'CR',
-	'AU', 'BR', 'RO', 'CL', 'MX', 'RU', 'ES', 'JP', 'CZ',
-	'PT', 'MT', 'AR', 'SE', 'PL', 'IE', 'BE', 'HT', 'NO',
-	'FI', 'GR', 'BO', 'AT', 'VE', 'PA', 'PE', 'GF', 'SA',
-	'CO', 'LT', 'NA', 'CH', 'CY', 'RS', 'KY', 'GP', 'DK',
-	'KR', 'LU', 'SV', 'VA', 'GT', 'SK', 'HR', 'ZA', 'DO',
-	'UY', 'LV', 'HN', 'JM', 'TR', 'IN', 'ER', 'AW', 'NZ',
-	'EC', 'TW', 'EE', 'CN', 'SI', 'AI', 'BG', 'NI', 'IS',
-	'MQ', 'BZ', 'BA', 'MY', 'AZ', 'ZW', 'AL', 'IM', 'VG',
-	'VI', 'BM', 'GY', 'SR', 'MS', 'TC', 'BB', 'TT', 'AG',
-	'BS', 'DM', 'GD', 'AN', 'PY', 'KN', 'LC', 'VC', 'BW',
-	'EE', 'LS', 'LI', 'MK', 'ME', 'MZ', 'SZ', 'ZM', 'MR',
-	'ML', 'NE', 'TD', 'SD', 'DJ', 'SO', 'AD', 'GI', 'IM',
-	'JE', 'MC', 'HK', 'MO', 'ID', 'SG', 'TH', 'PH', 'AE',
-	'EG', 'OM', 'QA', 'KW', 'SY', 'BH', 'JO', 'SM', 'VA'
+	'GB', 'US', 'IT', 'NL', 'DE', 'CA', 'FR', 'HU', 'CR', 'AU',
+	'BR', 'RO', 'CL', 'MX', 'RU', 'ES', 'JP', 'CZ', 'PT', 'MT',
+	'AR', 'SE', 'PL', 'IE', 'BE', 'HT', 'NO', 'FI', 'GR', 'BO',
+	'AT', 'VE', 'PA', 'PE', 'GF', 'SA', 'CO', 'LT', 'NA', 'CH',
+	'CY', 'RS', 'KY', 'GP', 'DK', 'KR', 'LU', 'SV', 'VA', 'GT',
+	'SK', 'HR', 'ZA', 'DO', 'UY', 'LV', 'HN', 'JM', 'TR', 'IN',
+	'ER', 'AW', 'NZ', 'EC', 'TW', 'EE', 'CN', 'SI', 'AI', 'BG',
+	'NI', 'IS', 'MQ', 'BZ', 'BA', 'MY', 'AZ', 'ZW', 'AL', 'IM',
+	'VG', 'VI', 'BM', 'GY', 'SR', 'MS', 'TC', 'BB', 'TT', 'AG',
+	'BS', 'DM', 'GD', 'AN', 'PY', 'KN', 'LC', 'VC', 'BW', 'LS',
+	'LI', 'MK', 'ME', 'MZ', 'SZ', 'ZM', 'MR', 'ML', 'NE', 'TD',
+	'SD', 'DJ', 'SO', 'AD', 'GI', 'JE', 'MC', 'HK', 'MO', 'ID',
+	'SG', 'TH', 'PH', 'AE', 'EG', 'OM', 'QA', 'KW', 'SY', 'BH',
+	'JO', 'SM'
 ];
 
 const LANGUAGES = [
