0a5f96ee2627b2f54572e6feb94b359c4ab75516
MisterSheeple <27652712+MisterSheeple@users.noreply.github.com>
2024-03-19 14:12:41 +0000

    task-finder: Use multiple tasks at once

--- spotpass/3ds-task-finder.js
+++ spotpass/3ds-task-finder.js
@@ -5,7 +5,7 @@ const database = require('./database');
 const apps = require('./ctr-boss-apps.json');
 
 const NPFL_URL_BASE = 'https://npfl.c.app.nintendowifi.net/p01/filelist';
-const TASK_SEARCH = 'FGONLYT'; // * For this example, I'm using the common FGONLYT task name, but there are other common task names that can be tested.
+const TASK_SEARCH = ['FGONLYT', 'news', 'data', 'TASK00', '0000001'] // * For this example, I'm using some common task names that are present in many games, but there are other ways this can be used, such as for matching tasks across different regions for a single game.
 
 const httpsAgent = new https.Agent({
 	rejectUnauthorized: false,
@@ -28,27 +28,30 @@ async function check3DS() {
 }
 
 async function findTask(task) {
-	const response = await axios.get(`${NPFL_URL_BASE}/${task.app_id}/${TASK_SEARCH}?c=${task.country}&l=${task.language}`, {
-		validateStatus: () => {
-			return true;
-		},
-		httpsAgent
-	});
+	for (const TASK of TASK_SEARCH) {
+		const response = await axios.get(`${NPFL_URL_BASE}/${task.app_id}/${TASK}?c=${task.country}&l=${task.language}`, {
+			validateStatus: () => {
+				return true;
+			},
+			httpsAgent
+		});
 
-	if (!response.headers['content-type'] || !response.headers['content-type'].startsWith('text/plain')) {
-		return;
+		if (!response.headers['content-type'] || !response.headers['content-type'].startsWith('text/plain')) {
+			return;
+		}
+	    for (const app of apps) {
+	    	if (app.app_id === task.app_id) {
+	    		if (!app.tasks.includes(TASK)) {
+	    			app.tasks.push(TASK);
+					fs.writeJSONSync('./ctr-boss-apps.json', apps, {
+						spaces: '\t'
+					});
+	    		}
+	    	}
+	    }
 	}
-        	for (const app of apps) {
-        		if (app.app_id === task.app_id) {
-        			if (!app.tasks.includes(TASK_SEARCH)) {
-        				app.tasks.push(TASK_SEARCH);
-						fs.writeJSONSync('./ctr-boss-apps.json', apps, {
-							spaces: '\t'
-						});
-        			}
-        		}
-        	}
 }
+
 
 async function find() {
 	await check3DS();
